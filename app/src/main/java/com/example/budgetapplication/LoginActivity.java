d2fbe6e067e5aa1b4afa6d8ccda1e25153fda122
kayatk <thillaikayatk@gmail.com>
2020-07-31 09:54:55 +0530

    Signup and login basic

--- app/src/main/java/com/example/budgetapplication/LoginActivity.java
+++ app/src/main/java/com/example/budgetapplication/LoginActivity.java
@@ -1,24 +1,41 @@
 package com.example.budgetapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView textView;
     TextView textView01;
     Button button;
+    EditText mEmail,mPassword;
+    FirebaseAuth fAuth;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mEmail= findViewById(R.id.userEmail);
+        mPassword= findViewById(R.id.pwd);
 
         textView = (TextView) findViewById(R.id.Signup);
         textView.setOnClickListener(new View.OnClickListener() {
@@ -28,16 +45,6 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        button = findViewById(R.id.loginBtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, Home.class);
-                startActivity(intent);
-            }
-        });
-
         textView01 = (TextView) findViewById(R.id.TVforgotPwd);
         textView01.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +55,42 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+        button = findViewById(R.id.loginBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email =mEmail.getText().toString().trim();
+                String password =mPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+                if (password.length()<6){
+                    mPassword.setError("Password must be greater or equal to 6 characters");
+                    return;
+                }
+
+                //Authenticate User
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),History.class) );
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Error !!" +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
 
     }
 }
--- app/src/main/java/com/example/budgetapplication/SignupActivity.java
+++ app/src/main/java/com/example/budgetapplication/SignupActivity.java
@@ -1,22 +1,48 @@
 package com.example.budgetapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
-
-     Button buttontoVerify;
+    EditText mUsername,mPhoneNo,mEmail,mPassword,mRePassword;
+    Button mSignUpBtn,mRadio_Single,mRadio_Married;
     TextView textView;
+    FirebaseAuth fAuth;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mUsername= findViewById(R.id.userNameSignup);
+        mPhoneNo= findViewById(R.id.userPhoneNo);
+        mEmail= findViewById(R.id.email);
+        mPassword= findViewById(R.id.pwdSignup);
+        mRePassword= findViewById(R.id.pwdReEnter);
+        mSignUpBtn=findViewById(R.id.nextBtn);
+        mRadio_Single=findViewById(R.id.radio_Single);
+        mRadio_Married=findViewById(R.id.radio_married);
+        fAuth=FirebaseAuth.getInstance();
 
         textView = (TextView)findViewById(R.id.Login);
         textView.setOnClickListener(new View.OnClickListener() {
@@ -26,17 +52,77 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //Already user is logged in
+        /*if (fAuth.getCurrentUser()!= null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class) );
+            finish();
+        }*/
 
-        buttontoVerify = findViewById(R.id.nextBtn);
-        buttontoVerify.setOnClickListener(new View.OnClickListener() {
+        mSignUpBtn.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
-                Intent intentVeri = new Intent(SignupActivity.this,UserVerify.class);
-                startActivity(intentVeri);
+                final String email =mEmail.getText().toString().trim();
+                String pwd =mPassword.getText().toString().trim();
+                String repwd=mRePassword.getText().toString().trim();
+                String username=mUsername.getText().toString().trim();
+                final Intent intent= new Intent(Intent.ACTION_SEND);;
+
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(pwd)){
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(repwd)){
+                    mRePassword.setError("ReEnter your Password.");
+                    return;
+                }
+                if (TextUtils.isEmpty(username)){
+                    mUsername.setError("Username is required.");
+                    return;
+                }
+                if (pwd.length()<6){
+                    mPassword.setError("Password must be greater or equal to 6 characters");
+                    return;
+                }
+                if (!pwd.equals(repwd)){
+                    mRePassword.setError("Mis Matching");
+                    return;
+                }
+
+                //Firebase
+                fAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            composeEmail(email,"Email verfification for budget friend");
+
+                            // Navigate to User Verification
+                            Intent intentVeri = new Intent(SignupActivity.this,UserVerify.class);
+                            startActivity(intentVeri);
+                        } else{
+                            Toast.makeText(SignupActivity.this, "Error !!" +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    public void composeEmail(String addresses, String subject) {
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        intent.setType("*/*");
+                        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                        //intent.putExtra(Intent.EXTRA_STREAM, attachment);
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                            startActivity(intent);
+                        }
+                    }
+                });
+
+
             }
         });
-
-
 
 
     }
